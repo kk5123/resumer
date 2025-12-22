33b8670ff1c7f94ee2b8043ae7cd00bcedacfe5f
kk5123 <mt68sakura@gmail.com>
2025-12-22 11:02:39 +0900

    [chore] Init UI shell

---
+++ apps/mobile/src/app/navigation/RootNavigator.tsx
@@ -0,0 +1,16 @@
+// navigation/RootNavigator.tsx
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function RootNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
