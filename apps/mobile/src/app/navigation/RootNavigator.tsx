ff6dd364d5c0aaac40ce9cbd508080aebe017be9
kk5123 <mt68sakura@gmail.com>
2026-01-02 14:47:22 +0900

    [feat] Add bottom tab navigation

--- apps/mobile/src/app/navigation/RootNavigator.tsx
+++ apps/mobile/src/app/navigation/RootNavigator.tsx
@@ -1,16 +1,21 @@
 // navigation/RootNavigator.tsx
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 import HomeScreen from '../screens/HomeScreen';
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function RootNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ tabBarLabel: 'ホーム' }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
